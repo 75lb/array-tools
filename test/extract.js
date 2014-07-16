a4e37288e2f783a1bdd6bdba88261415ebe230a9
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-16 10:49:49 +0100

    .extract() now works with exists query objects

--- test/extract.js
+++ test/extract.js
@@ -22,3 +22,22 @@ test("extract where test returns true", function(t){
     ]);
     t.end();
 });
+
+test("extract where query matches", function(t){
+    var arr = [
+        { result: false, number: 1 },
+        { result: false, number: 2 },
+        { result: true, number: 3 },
+        { result: true, number: 4 }
+    ];
+    var result = a.extract(arr, { result: false});
+    t.deepEqual(result, [
+        { result: false, number: 1 },
+        { result: false, number: 2 }
+    ]);
+    t.deepEqual(arr, [
+        { result: true, number: 3 },
+        { result: true, number: 4 }
+    ]);
+    t.end();
+});
