0406357d05dade7355f10ebb661492c5c0e3530d
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-21 19:15:46 +0000

    commonSequence fix

--- test/without.js
+++ test/without.js
@@ -1,8 +1,8 @@
 var test = require("tape"),
-    w = require("../");
+    a = require("../");
 
 test("without (array)", function(t){
     var array = [ 1,2,3,4 ];
-    t.deepEqual(w.without(array, [ 2, 3 ]), [ 1, 4 ]);
+    t.deepEqual(a.without(array, [ 2, 3 ]), [ 1, 4 ]);
     t.end();
 });
