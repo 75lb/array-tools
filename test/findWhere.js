d709cb7801d40bef3079d39bd5a5ceb13d880aeb
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-05-18 18:09:47 +0100

    added tests for deep querying in .where and .findWhere

--- test/findWhere.js
+++ test/findWhere.js
@@ -1,5 +1,5 @@
-var test = require("tape"),
-    a = require("../");
+var test = require("tape");
+var a = require("../");
 
 test("findWhere", function(t){
     var arr = [
@@ -10,5 +10,17 @@ test("findWhere", function(t){
     t.deepEqual(a.findWhere(arr, { result: false }), { result: false, number: 1 });
     t.deepEqual(a.findWhere(arr, { result: false, number: 3 }), null);
     t.deepEqual(a.findWhere(arr, { result: false, number: 2 }), { result: false, number: 2 });
+    t.end();
+});
+
+test(".findWhere deep query", function(t){
+    var arr = [
+        { one: { number: 1, letter: "a" } },
+        { one: { number: 2, letter: "b" } }
+    ];
+    t.deepEqual(a.findWhere(arr, { one: { number: 1 } }), { one: { number: 1, letter: "a" } });
+    t.deepEqual(a.findWhere(arr, { one: { number: 2 } }), { one: { number: 2, letter: "b" } });
+    t.deepEqual(a.findWhere(arr, { one: { letter: "b" } }), { one: { number: 2, letter: "b" } });
+    t.deepEqual(a.findWhere(arr, { one: { number: 3 } }), null);
     t.end();
 });
--- test/where.js
+++ test/where.js
@@ -1,5 +1,5 @@
-var test = require("tape"),
-    a = require("../");
+var test = require("tape");
+var a = require("../");
 
 test("where", function(t){
     var arr = [
@@ -39,5 +39,24 @@ test("where, regex 2", function(t){
         { something: "bb" }
     ];
     t.deepEqual(a.where(arr, { something: /.+/ }), result);
+    t.end();
+});
+
+test(".where deep query", function(t){
+    var arr = [
+        { one: { number: 1, letter: "a" } },
+        { one: { number: 2, letter: "b" } },
+        { one: { number: 3, letter: "b" } }
+    ];
+    t.deepEqual(a.where(arr, { one: { letter: "b" } }), [
+        { one: { number: 2, letter: "b" } },
+        { one: { number: 3, letter: "b" } }
+    ]);
+    t.deepEqual(a.where(arr, { one: { number: 2, letter: "b" } }), [
+        { one: { number: 2, letter: "b" } }
+    ]);
+    t.deepEqual(a.where(arr, { one: { number: 1, letter: "a" } }), [
+        { one: { number: 1, letter: "a" } }
+    ]);
     t.end();
 });
