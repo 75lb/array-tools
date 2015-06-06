380fee7eb8452e129da38b7f45a08ed0d3f29869
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-06 21:20:20 +0300

    .exists now accepts an array of primitives as a query

--- test/exists.js
+++ test/exists.js
@@ -1,32 +1,33 @@
 var test = require("tape");
 var a = require("../");
 
-test("exists: query a recordset", function(t){
-    var arr = [
+var f = {
+    recordset: [
         { result: false, number: 1 },
         { result: false, number: 2 }    
-    ];
-    t.equal(a.exists(arr, { result: true }), false);
-    t.equal(a.exists(arr, { result: false }), true);
-    t.equal(a.exists(arr, { result: false, number: 3 }), false);
-    t.equal(a.exists(arr, { result: false, number: 2 }), true);
+    ],
+    arr: [ 1, 2, "three" ]
+};
+
+test(".exists(recordset, query)", function(t){
+    t.equal(a.exists(f.recordset, { result: true }), false);
+    t.equal(a.exists(f.recordset, { result: false }), true);
+    t.equal(a.exists(f.recordset, { result: false, number: 3 }), false);
+    t.equal(a.exists(f.recordset, { result: false, number: 2 }), true);
     t.end();
 });
 
-test("exists: search for scalar", function(t){
-    var arr = [ 1, 2, "three" ];
-    t.equal(a.exists(arr, 0), false);
-    t.equal(a.exists(arr, 1), true);
-    t.equal(a.exists(arr, "1"), false);
-    t.equal(a.exists(arr, "three"), true);
+test(".exists(array, primitive)", function(t){
+    t.equal(a.exists(f.arr, 0), false);
+    t.equal(a.exists(f.arr, 1), true);
+    t.equal(a.exists(f.arr, "1"), false);
+    t.equal(a.exists(f.arr, "three"), true);
     t.end();
 });
 
-test.skip("exists: search an object", function(t){
-    var obj1 = {};
-    var obj2 = {};
-    var arr = [ obj1, {} ];
-    t.equal(a.exists(arr, obj1), true);
-    t.equal(a.exists(arr, obj2), false);
+test(".exists(array, primitive[])", function(t){
+    t.equal(a.exists(f.arr, [ 1, 2 ]), true);
+    t.equal(a.exists(f.arr, [ 1, 2, 3 ]), false);
+    t.equal(a.exists(f.arr, [ 1, 2, "three" ]), true);
     t.end();
 });
