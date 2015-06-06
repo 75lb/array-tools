4c47a2e4b0d43b5d11b82ad5e2eba463dce59f34
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-06 14:18:52 +0300

    upgrade cli to use transform-tools

--- bin/cli.js
+++ bin/cli.js
@@ -2,22 +2,24 @@
 "use strict";
 
 var a = require("../");
+var tr = require("transform-tools");
+
+if (process.argv.length < 4){
+    console.error("Usage:");
+    console.error("$ cat <json array> | array-tools <method> <args...>");
+    process.exit(1);
+}
 
 process.argv.splice(0, 2);
-
-var input = new Buffer(0);
 var method = process.argv.shift();
 var args = process.argv.slice(0);
 
-process.stdin.on("readable", function(){
-    var chunk = this.read();
-    if (chunk) input = Buffer.concat([ input, chunk ]);
-});
-process.stdin.on("end", function(){
-    var array = JSON.parse(input);
+function processInput(array){
     args.unshift(array);
-    if (method){
-        var result = a[method].apply(null, args);
-        console.log(JSON.stringify(result, null, "  "))
-    }
-});
+    var result = a[method].apply(null, args);
+    return JSON.stringify(result, null, "  ") + "\n";
+}
+
+process.stdin
+    .pipe(tr.collectJson({ transform: processInput }))
+    .pipe(process.stdout);
