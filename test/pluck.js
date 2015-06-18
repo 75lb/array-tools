6f32543d4a646163ccfb70d8ac29be7fc1207925
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-18 10:39:06 +0100

    updated .pluck

--- test/pluck.js
+++ test/pluck.js
@@ -1,29 +1,28 @@
 var test = require("tape");
 var a = require("../");
 
-test(".pluck", function(t){
-    var data = [
-        {one: 1, two: 2},
-        {two: "two"},
-        {one: "one", two: "zwei"},
-    ];
-    
-    t.deepEqual(a.pluck(data, "one"), [ 1, "one" ]);
-    t.deepEqual(a.pluck(data, "two"), [ 2, "two", "zwei" ]);
-    t.deepEqual(a.pluck(data, "one", "two"), [ 1, "two", "one" ]);
+var fixture = [
+    {one: 1, two: 2},
+    {two: "two"},
+    {one: "one", two: "zwei"},
+    {deep: { a: "yep" }},
+    {deep: { a: "again" }},
+    {deep: "not here" },
+    {deep: { b: "or here" }}
+];
 
+test(".pluck(array, property)", function(t){
+    t.deepEqual(a.pluck(fixture, "one"), [ 1, "one" ]);
+    t.deepEqual(a.pluck(fixture, "two"), [ 2, "two", "zwei" ]);
     t.end();
 });
 
-test(".pluck expression (e.g. \"item.prop\")", function(t){
-    var data = [
-        {one: { inside: "yep" }},
-        {one: { inside: "again" }},
-        {one: "not here" },
-        {one: { or: "or here" }}
-    ];
-    
-    t.deepEqual(a.pluck(data, "one.inside"), [ "yep", "again" ]);
+test(".pluck(array, [property])", function(t){
+    t.deepEqual(a.pluck(fixture, [ "one", "two" ]), [ 1, "two", "one" ]);
+    t.end();
+});
 
+test(".pluck(array, property.property)", function(t){
+    t.deepEqual(a.pluck(fixture, "deep.a"), [ "yep", "again" ]);
     t.end();
 });
