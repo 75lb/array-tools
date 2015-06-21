a08d9b0ca30c2b08efcdcfddecc420efe50ac587
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-21 15:06:27 +0100

    refactor union tests

--- test/union.js
+++ test/union.js
@@ -1,21 +1,27 @@
 var test = require("tape");
 var a = require("../");
 
+var f = {
+    arr1: [ 1, 2, 3 ],
+    arr2: [ 3, 4, 5 ],
+    arr3: [ { id: 1 }, { id: 2 }, { id: 3 } ],
+    arr4: [ { id: 3 }, { id: 4 }, { id: 5 } ],
+    arr5: [ { id: 3, blah: true }, { id: 4 }, { id: 5 } ]
+};
+
 test(".union(array, array)", function(t){
-    var arr1 = [1,2,3],
-        arr2 = [3,4,5];
-    t.deepEqual(a.union(arr1, arr2), [1,2,3,4,5]);
+    t.deepEqual(a.union(f.arr1, f.arr2), [ 1, 2, 3, 4, 5 ]);
     t.end();
 });
 
-test(".union(arrayOfObjects, arrayOfObjects, idKey)", function(t){
-    var arr1 = [ { id: 1 }, { id: 2 }, { id: 3 }],
-        arr2 = [ { id: 3 }, { id: 4 }, { id: 5 }];
-    t.deepEqual(a.union(arr1, arr2), [ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 } ]);
+test(".union(recordset, recordset, idKey)", function(t){
+    t.deepEqual(a.union(f.arr3, f.arr4), [ 
+        { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 } 
+    ]);
 
-    arr1 = [ { id: 1 }, { id: 2 }, { id: 3 }];
-    arr2 = [ { id: 3, blah: true }, { id: 4 }, { id: 5 }];
-    t.deepEqual(a.union(arr1, arr2, "id"), [ { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 } ]);
+    t.deepEqual(a.union(f.arr3, f.arr5, "id"), [ 
+        { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 } 
+    ]);
 
     t.end();
 });
